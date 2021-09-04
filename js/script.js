c23f24685e5add252938e04b508bca85ad523920
Evgeny <Example@mail.ru>
2021-09-04 10:16:49 +0300

    Изменил скрипт часть2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
-//const result = confirm("Are you here?");
-//console.log(result);
+const result = confirm("Are you here?");
+console.log(result);
 
-// const answer = +prompt("Вам есть 18?", "18");
-// console.log(typeof(answer));
+const answer = +prompt("Вам есть 18?", "18");
+console.log(typeof(answer));
 
 const answers = [];
 answers[0] = prompt('Как ваше имя?','');
