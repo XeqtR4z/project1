9614abf3bdbd81100633b9d77aaf1ac71727ab28
Evgeny <Example@mail.ru>
2021-09-04 10:11:15 +0300

    Изменил скрипт

--- js/script.js
+++ js/script.js
@@ -7,8 +7,21 @@
 // console.log(typeof(answer));
 
 const answers = [];
-// answers[0] = prompt('Как ваше имя?','');
-// answers[1] = prompt('Как ваша фамилия?','');
-// answers[2] = prompt('Сколько вам лет?','');
+answers[0] = prompt('Как ваше имя?','');
+answers[1] = prompt('Как ваша фамилия?','');
+answers[2] = prompt('Сколько вам лет?','');
 
 console.log(typeof(answers));
+
+const category = 'toys';
+console.log(`https://someutl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет, ${user}`);
+
+let incr = 10;
+let decr = 10;
+incr++;
+++incr;
+decr--;
+--decr;
