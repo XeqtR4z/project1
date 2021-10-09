eead6af1963e0642aff04b094eabd826f1e5104f
Evgeny <zhek.14@yandex.ru>
2021-10-09 19:43:05 +0300

    64 localStorage

--- preview/script.js
+++ preview/script.js
@@ -1,34 +1,36 @@
-"use strict";
+'use strict';
 
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
-// btn.onclick = function(){ Так делать не стоит
-//     alert('Click');
-// };
-// btn.addEventListener('click', ()=>{//добавляем обработчик события , нажатие
-//     alert('Click');
-// });
-//let i = 0;
-const deleteElement = (event) =>{
-    //event.target.remove();
-    console.log(event.currentTarget);
-    console.log(event.type);
-    // i++;
-    // if(i == 1){
-    //     btn.removeEventListener('mouseenter',deleteElement);
-    // }
+// localStorage.setItem('number', 5);
+// localStorage.removeItem('number');
+// localStorage.clear();
+// console.log(localStorage.getItem('number'));
+
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
+
+if(localStorage.getItem('isCheck')){
+    checkbox.checked = true;
+} 
+if(localStorage.getItem('bg') === 'changed'){
+    form.style.backgroundColor = 'red';
+}     
+checkbox.addEventListener('change', () =>{
+    localStorage.setItem('isCheck', true);
+});
+change.addEventListener('click', () => {
+    if(localStorage.getItem('bg') === 'changed'){
+        form.style.backgroundColor = '#fff';
+        localStorage.removeItem('bg'); 
+    }else{
+       localStorage.setItem('bg', 'changed');
+       form.style.backgroundColor = 'red'; 
+    }
+});
+const presone = {
+    name:"Alex",
+    age: 25
 };
-// btn.addEventListener('mouseenter', (event)=>{//наводим на элемент
-//     console.log(event.targer);//target получаем элемент на который навели
-//     //event.target.remove();
-// });
-// btn.addEventListener('click',deleteElement);
-// overlay.addEventListener('click',deleteElement);
-btns.forEach(btn =>{
-    btn.addEventListener('click',deleteElement, {once: true});
-});
-const link = document.querySelector('a');
-link.addEventListener('click', (event) => {
-    event.preventDefault();//отменяет все действия, который должны были произойти по клику на объект
-    console.log(event.target);
-});
+const serializedPersone = JSON.stringify(presone);
+localStorage.setItem('alex', serializedPersone);
+console.log(JSON.parse(localStorage.getItem('alex')));
